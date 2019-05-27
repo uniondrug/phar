b53ac8f88dbccae477e81c5125abc69a4ecdad3c
fuyibing <websearch@163.com>
2019-05-27 23:40:51 +0800

    Update EventsTrait.php
    取消内存128M时自动退出进程

--- src/Server/Services/Traits/EventsTrait.php
+++ src/Server/Services/Traits/EventsTrait.php
@@ -116,7 +116,9 @@ trait EventsTrait
         // 5. 退出Worker进程
         //    当worker进程占用的内存资源达到临界值时
         //    主动发起退出进程请求
-        $memoryLimit && $server->stop($server->getWorkerId(), true);
+        // date: 2019-05-27
+        // 以下代码会造成 cURL timed out, 暂时取消主动退出进程操作
+        // $memoryLimit && $server->stop($server->getWorkerId(), true);
     }
 
     /**
